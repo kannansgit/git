41b0bd90cd6497a5c0d14649f115244520e30a3b
kannansgit <lakshminarayananm30@gmailcom>
2025-09-16 14:34:16 +0530

    hi

--- summa.java
+++ summa.java
@@ -2,5 +2,6 @@ public class summa {
     public static void main(String[] args){
         System.out.println("git");
         System.out.println("changes made in local repo");
+        System.out.println("changes made in local");
     }
 }
